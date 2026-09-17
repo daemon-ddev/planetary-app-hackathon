e1d96775d7c38abb20ead3f18301d31e2d985fe3
daemon-ddev <guledjama.dev@gmail.com>
2026-09-17 18:09:08 +0100

    Lock SSH to my own IP

--- terraform/infrastructure/network-security-group.tf
+++ terraform/infrastructure/network-security-group.tf
@@ -29,7 +29,7 @@ resource "azurerm_network_security_rule" "ssh_ingress" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "22"
-  source_address_prefix       = "0.0.0.0/0"
+  source_address_prefix       = "37.156.72.128/32"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.vm_resource_group.name
   network_security_group_name = azurerm_network_security_group.http_server_nsg.name
