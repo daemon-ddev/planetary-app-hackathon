8a628b6c246c037e6d4805a63c7d37833b3746dd
daemon-ddev <guledjama.dev@gmail.com>
2026-09-17 16:39:45 +0100

    Planetary_app_hackathon_API

--- server/models/PlanetaryAppHackathon.js
+++ server/models/PlanetaryAppHackathon.js
@@ -2,8 +2,13 @@ const db = require("../db/connect");
 
 class PlanetaryAppHackathon {
   static async findAll() {
-    const result = await db.query("SELECT * FROM planetary_app_hackathon");
+    const result = await db.query("SELECT * FROM planetary_app_hackathon ORDER BY id");
     return result.rows;
+  }
+
+  static async findById(id) {
+    const result = await db.query("SELECT * FROM planetary_app_hackathon WHERE id = $1", [id]);
+    return result.rows[0];
   }
 }
 
